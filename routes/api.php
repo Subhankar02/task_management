ea0f660f48ed8cd9441d6ac905cd7d9501d0564d
SubhankarOfficial02 <subhankarsharma.official02@gmail.com>
2023-12-29 16:01:40 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => ['auth:sanctum']], function (){
 
 Route::post('/add/task',                    [TaskController::Class, 'add_task']);
 Route::get('/get/users',                    [TaskController::Class, 'get_users']);
-Route::get('/get/tasks/{task_id}',          [TaskController::Class, 'get_tasks']);
+Route::get('/get/tasks/{task_date}',          [TaskController::Class, 'get_tasks']);
 Route::get('/get/a_task/{task_id}',         [TaskController::Class, 'get_a_task']);
 Route::get('/delete/task/{task_id}',        [TaskController::Class, 'delete_task']);
 Route::post('/update/task',                 [TaskController::Class, 'update_task']);
